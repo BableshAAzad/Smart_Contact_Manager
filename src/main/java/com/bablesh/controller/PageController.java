8f3ec9ce0ce57ca4e3b4f3b2788c700852ad8b4f
bableshaazad <bableshaazad@outlook.com>
2024-05-11 22:03:35 +0530

    fragment basics

--- src/main/java/com/bablesh/controller/PageController.java
+++ src/main/java/com/bablesh/controller/PageController.java
@@ -2,15 +2,31 @@ package com.bablesh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    @RequestMapping("/")
+    // ^ home page
+    @GetMapping("/")
     public String home(Model model) {
         // * Sending data to view
         model.addAttribute("name", "Bablesh AAzad");
         model.addAttribute("Address", "Raipur");
         return "home";
     }
+
+    // ^ about page
+    @GetMapping("/about")
+    public String aboutPage() {
+        System.out.println("About Page loading.....");
+        return "about";
+    }
+
+    // ^ service page
+    @GetMapping("/services")
+    public String servicesPage() {
+        System.out.println("Services page loading.....");
+        return "services";
+    }
+
 }
