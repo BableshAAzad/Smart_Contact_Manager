2d3eb91cb79b4a3641ef37f1a1b4bf103c0f8e7f
bableshaazad <bableshaazad@outllok.com>
2024-08-30 16:33:04 +0530

    add readme file 🧑‍💻🤴

--- src/main/java/com/bablesh/service/impl/ContactServiceImpl.java
+++ src/main/java/com/bablesh/service/impl/ContactServiceImpl.java
@@ -16,9 +16,7 @@ import com.bablesh.repository.ContactRepo;
 import com.bablesh.service.ContactService;
 
 @Service
-public class ContactServiceImpl implements ContactService
-
-{
+public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepo contactRepo;
@@ -34,8 +32,21 @@ public class ContactServiceImpl implements ContactService
 
     @Override
     public Contact update(Contact contact) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+
+        var contactOld = contactRepo.findById(contact.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Contact not found"));
+        contactOld.setName(contact.getName());
+        contactOld.setEmail(contact.getEmail());
+        contactOld.setPhoneNumber(contact.getPhoneNumber());
+        contactOld.setAddress(contact.getAddress());
+        contactOld.setDescription(contact.getDescription());
+        contactOld.setPicture(contact.getPicture());
+        contactOld.setFavorite(contact.isFavorite());
+        contactOld.setWebsiteLink(contact.getWebsiteLink());
+        contactOld.setLinkedInLink(contact.getLinkedInLink());
+        contactOld.setCloudinaryImagePublicId(contact.getCloudinaryImagePublicId());
+
+        return contactRepo.save(contactOld);
     }
 
     @Override
